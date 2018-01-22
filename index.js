ae58ce205550ed929be2a01ebba29581639fdaf4
dzimnitsky <dzimnitsky@dev-team.com>
2018-01-22 17:51:22 +0300

    add weather

--- index.js
+++ index.js
@@ -2,6 +2,48 @@ var TelegramBot = require('node-telegram-bot-api');
 var Cron = require('cron').CronJob;
 var request = require('request');
 
+var config = {
+    APPID: "d29939260bc59b9109c0121f43f43774",
+    units: "metric",
+    cityId: "625144",
+
+};
+
+var minsk = {};
+
+var getWeather = function () {
+    request.get(`https://api.openweathermap.org/data/2.5/weather?id=${config.cityId}&units=${config.units}&APPID=${config.APPID}`,
+        (err, data) => {
+            let error = null;
+            let response = null;
+
+            error = err;
+            if (data) {
+                if (data.statusCode != 200) {
+                    let e = JSON.parse(data.body)
+                    error = new Error(e.message);
+                    error.status = e.cod;
+                }
+                if (data.body) {
+                    response = JSON.parse(data.body);
+                }
+            };
+            ((err, currentWeather) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    minsk.mainTemp = currentWeather.main.temp;
+                }
+            })(error, response);
+        }
+    );
+};
+
+getWeather();
+
+setInterval(getWeather, 60000);
+
 var token = '361991855:AAGD2iKkVtbyHhKbvqCEGKdE994UxejXyto';
 
 var bot = new TelegramBot(token, {
@@ -18,6 +60,10 @@ var Nadya = {
     id: '147774430'
 };
 
+var Zhenya = {
+    id: '371525126'
+}
+
 var optionsMe = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
@@ -31,6 +77,12 @@ var optionsMe = {
                 {
                     text: 'Надя',
                     callback_data: '3'
+                }
+            ],
+            [
+                {
+                    text: 'Женя',
+                    callback_data: '4'
                 }
             ]
         ]
@@ -74,7 +126,18 @@ var optionsNadya = {
     })
 };
 
-
+var optionsZhenya = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [
+                {
+                    text: 'Димон',
+                    callback_data: '1'
+                }
+            ]
+        ]
+    })
+};
 
 var optionsTime = {
     reply_markup: JSON.stringify({
@@ -89,12 +152,13 @@ var optionsTime = {
     })
 }
 
-
 bot.on('message', function (msg, match) {
     this.msg = msg;
     var id = msg.from.id;
     switch (msg.text === 'привет') {
         case msg.from.id == Dima_Z.id:
+            // console.log(minsk.mainTemp);
+            bot.sendMessage(Dima_Z.id, 'норм погода , чтобы потусить ' + minsk.mainTemp + ' по цельсию');
             bot.sendMessage(Dima_Z.id, 'привет Димон, едем на алми?', optionsMe);
             bot.on("callback_query", function (callbackQuery) {
                 if (callbackQuery.data == '2') {
@@ -103,6 +167,9 @@ bot.on('message', function (msg, match) {
                 } else if (callbackQuery.data == '3') {
                     bot.sendMessage(Dima_Z.id, 'приглос отправлен Наде');
                     bot.sendMessage(Nadya.id, 'Димка Зимницкий зовёт на алми');
+                } else if  (callbackQuery.data == '4'){
+                    bot.sendMessage(Dima_Z.id, 'приглос отправлен Жене');
+                    bot.sendMessage(Zhenya.id, 'Димас зовёт на алми');
                 }
             });
             break;
@@ -119,6 +186,7 @@ bot.on('message', function (msg, match) {
             });
             break;
         case msg.from.id == Dima_Solo.id:
+            bot.sendMessage(Dima_Solo.id, 'норм погода , чтобы потусить ' + minsk.mainTemp + ' по цельсию');
             bot.sendMessage(Dima_Solo.id, 'привет Димac, едем на алми?', optionsDima);
             bot.on("callback_query", function (callbackQuery) {
                 if (callbackQuery.data == '1') {
@@ -130,6 +198,17 @@ bot.on('message', function (msg, match) {
                 }
             });
             break;
+        case msg.from.id == Zhenya.id:
+            bot.sendMessage(Zhenya.id, 'привет Женя, едем на алми?', optionsZhenya);
+            bot.on("callback_query", function (callbackQuery) {
+                if (callbackQuery.data == '1') {
+                    bot.sendMessage(Zhenya.id, 'приглос отправлен Димасу');
+                    bot.sendMessage(Dima_Z.id, 'Женя зовёт на алми');
+                } else if (callbackQuery.data == '2') {
+
+                }
+            });
+            break
         default:
             bot.sendMessage(id, 'привет ' + msg.from.first_name + ', едем на алми?', options);
     }
